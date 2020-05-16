ba7f9f877982f8e03ade00fad8d302b365c3d31c
Roman Jaster <roman@yaybrigade.com>
2020-05-15 17:17:46 -0700

    Fix issue with some image not showing if there's only one:
    One more issue (sorry haha), on most, but somehow not all pages, the project 3 tab needs you to click next in order for the single picture to show up. Here's an example: https://makeitbig.calarts.edu/html/student/bfa3/joe-suh.html
    Joe's Children of Men poster doesn't show up, but it's there and none of the code is any different.
    
    I have been putting two of the same images in some of these pages because then it loads, but if there is a proper fix let me know.

--- js/scripts.js
+++ js/scripts.js
@@ -47,7 +47,7 @@ document.getElementById("defaultOpen").click();
 
 // Template Image Slideshow //
 
-var slideIndex = [1,1,2,3];
+var slideIndex = [1,1,1,1];
 var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
 showSlides(1, 0);
 showSlides(1, 1);
@@ -61,12 +61,14 @@ function plusSlides(n, no) {
 function showSlides(n, no) {
   var i;
   var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+  if (x.length > 0) {
+    if (n > x.length) {slideIndex[no] = 1}    
+    if (n < 1) {slideIndex[no] = x.length}
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    x[slideIndex[no]-1].style.display = "block";  
   }
-  x[slideIndex[no]-1].style.display = "block";  
 }
 
 
